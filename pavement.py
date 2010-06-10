c9ae7f6bdd228a020dae6ed8321dd3c35a4ed78f
Travis J Parker <travis.parker@gmail.com>
2010-06-10 16:34:45 -0700

    git ignores, license, package distribution stuff

---
+++ pavement.py
@@ -0,0 +1,64 @@
+import errno
+import os
+from setuptools import Extension
+
+from paver.easy import *
+from paver.path import path
+from paver.setuputils import setup
+
+
+setup(
+    name="green_ssl",
+    description="socket.ssl monkeypatching that cooperates in gogreen",
+    version="1.0",
+    license="bsd",
+    ext_modules=[Extension(
+        'green_ssl',
+        ['green_ssl_module.c'],
+        include_dirs=('.',),
+        libraries=['ssl', 'crypto'],
+        extra_compile_args=['-Wall'])],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Programming Language :: C",
+        "Topic :: Security :: Cryptography",
+        "Topic :: System :: Networking",
+    ]
+)
+
+MANIFEST = (
+    "LICENSE",
+    "setup.py",
+    "paver-minilib.zip",
+    "green_ssl_module.c",
+)
+
+@task
+def manifest():
+    path('MANIFEST.in').write_lines('include %s' % x for x in MANIFEST)
+
+@task
+@needs('generate_setup', 'minilib', 'manifest', 'setuptools.command.sdist')
+def sdist():
+    pass
+
+@task
+def clean():
+    for p in map(path, ('green_ssl.egg-info', 'dist', 'build', 'MANIFEST.in')):
+        if p.exists():
+            if p.isdir():
+                p.rmtree()
+            else:
+                p.remove()
+    for p in path(__file__).abspath().parent.walkfiles():
+        if p.endswith(".pyc") or p.endswith(".pyo"):
+            try:
+                p.remove()
+            except OSError, exc:
+                if exc.args[0] == errno.EACCES:
+                    continue
+                raise
